f9e1d3081c3ee481b837215e1b72fbc3a4b370d9
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-04-11 12:57:55 +0430

    Add test projects

---
+++ BafghAutomation.Engine.Tests/Tests.fs
@@ -0,0 +1,11 @@
+namespace BafghAutomation.Engine.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
