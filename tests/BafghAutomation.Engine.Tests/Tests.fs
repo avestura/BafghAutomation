92d5d9fe533d5714ce14ec6026ddca42336ca19c
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-04-11 13:43:56 +0430

    Update test project

--- tests/BafghAutomation.Engine.Tests/Tests.fs
+++ tests/BafghAutomation.Engine.Tests/Tests.fs
@@ -1,11 +1,17 @@
-namespace BafghAutomation.Engine.Tests
+module Tests
 
 open System
-open Microsoft.VisualStudio.TestTools.UnitTesting
+open Xunit
+open BafghAutomation.Engine
 
-[<TestClass>]
-type TestClass () =
+type FunFact = FactAttribute
 
-    [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+[<FunFact>]
+let ``Date from string returns correct result`` () =
+    Assert.True (Utils.GetDateFromString "13980110" = struct("1398", "01", "10"))
+    Assert.True (Utils.GetDateFromString "13761001" = struct("1376", "10", "01"))
+
+[<FunFact>]
+let ``Time from string returns correct result`` () =
+    Assert.True (Utils.GetTimeFromString "1020" = struct("10", "20"))
+    Assert.True (Utils.GetTimeFromString "2230" = struct("22", "30"))
