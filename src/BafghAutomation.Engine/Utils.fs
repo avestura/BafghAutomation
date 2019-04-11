1104dd968cdf9d95ad95c9f4b4a32db297adf922
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-04-11 13:42:05 +0430

    Util bug fixed

--- src/BafghAutomation.Engine/Utils.fs
+++ src/BafghAutomation.Engine/Utils.fs
@@ -2,7 +2,7 @@
 module Utils =
 
     let GetDateFromString (str:string) =
-        struct (str.[0..3], str.[4..5], str.[5..6])
+        struct (str.[0..3], str.[4..5], str.[6..7])
 
     let GetTimeFromString (str:string) =
         struct (str.[0..1], str.[2..3])
